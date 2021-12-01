1e979784f54a5fc28f51e3f8f2b9ac577c9879f8
Martin Kase <mail@kasemartin.cz>
2021-12-01 22:50:57 +0100

    Update

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express(),
-    port = 3000;
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  app = express(),
+  port = 3000;
+const axios = require("axios");
 
 app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
-    var body = req.body;
-    var trackingNumber = body.msg.tracking_number;
-    var slug = body.msg.slug;
-    var token = body.msg.unique_token;
+app.post("/webhook", function (req, res) {
+  var body = req.body;
 
-    console.log(trackingNumber, slug, token);
+  var token = req.headers.authorization;
 
-    res.json({
-        message: 'ok got it!'
+  axios
+    .post(
+      "https://api.github.com/repos/kasik96/vimto-landpage-frontend/actions/workflows/azure-static-web-apps-gray-sky-0ba056e03.yml/dispatches",
+      { ref: "main" },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      }
+    )
+    .then((res) => {
+      console.log(`statusCode: ${res.status}`);
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
     });
+
+  res.json({
+    message: "ok got it!",
+  });
 });
 
 var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log('Example app listening at http://%s:%s', host, port)
-
+  console.log("Example app listening at http://%s:%s", host, port);
 });
